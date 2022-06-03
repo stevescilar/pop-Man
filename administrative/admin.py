651c31be6368dc50171cb98d470b7cef8a236b9d
stevescilar <stevemuambi@gmail.com>
2022-06-03 15:57:05 +0300

    update

--- administrative/admin.py
+++ administrative/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from . models import Student,Class,Subject
 # Register your models here.
+admin.site.register(Student)
+admin.site.register(Class)
+admin.site.register(Subject)
--- administrative/models.py
+++ administrative/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
 # Create your models here.
+class Class(models.Model):
+    name = models.CharField(max_length=50, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True,blank=True,null=True)
+    updated_at = models.DateTimeField(auto_now=True,blank=True,null=True)
+    def __str__(self):
+            return self.name
+    
+    class Meta:
+        verbose_name = 'Class'
+        verbose_name_plural = 'Classes'
+
+class Subject(models.Model):
+        name = models.CharField(max_length=50, blank=True)
+        subject_class = models.ForeignKey(Class, verbose_name="Class", on_delete=models.CASCADE)
+        created_at = models.DateTimeField(auto_now_add=True,blank=True,null=True)
+        updated_at = models.DateTimeField(auto_now=True,blank=True,null=True)
+
+        def __str__(self):
+            return self.name 
+
+class Student(models.Model):
+    name = models.CharField(max_length=50, blank=True)
+    age = models.CharField(max_length=40, blank=True)
+    student_class = models.ForeignKey(Class, verbose_name=("Class"), on_delete=models.CASCADE,null=True) 
+    stream = models.CharField(max_length=20, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True,blank=True,null=True)
+    updated_at = models.DateTimeField(auto_now=True,blank=True,null=True)
+
+    def __str__(self):
+            return self.name
+
+
+class Parent(models.Model):
+    name = models.CharField(max_length=50, blank=True)
+    student_name = models.ForeignKey(Student, on_delete=models.CASCADE,null=True)
+    created_at = models.DateTimeField(auto_now_add=True,blank=True,null=True)
+    updated_at = models.DateTimeField(auto_now=True,blank=True,null=True)
+
+    def __str__(self):
+            return self.name
