8c1a4b89c1753cdc8ca3f23505b22e347d9ad850
stevescilar <stevemuambi@gmail.com>
2022-06-07 13:36:17 +0300

    update

--- administrative/views.py
+++ administrative/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-
+from . models import Student
 # Create your views here.
 def dashboard(request):
-
-    return render(request, 'administrative/index.html')
+    total_students = Student.objects.all()
+    context = {
+        'total_students':total_students
+    }
+    return render(request, 'administrative/index.html',context)
